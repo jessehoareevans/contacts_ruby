0cb6dc5f3ab845a22ef1d2a0dac97e018ed89f12
Brian Fan and Jesse Hoare Evans <jesse@>
2017-04-27 16:48:43 -0700

    add functioning integration spec

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -5,8 +5,9 @@ set(:show_exceptions, false)
 
 describe('the contact path', {:type => :feature}) do
   it('displays the id of added contacts after the user submits the form') do
-    visit('contact_form')
-    fill_in('first_name', :with => 'Jesse', 'last_name', :with => 'Evans', 'job', :with => 'Student', 'company' :with => 'Epicodus')
+    visit('/new_contact')
+    fill_in('first_name', :with => "Jesse")
     click_button('Add Contact')
+    expect(page).to have_content('Contacts')
   end
 end
